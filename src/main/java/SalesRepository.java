86ff0ddcedabf5b4b2b9b9d9f410cac6d968d683
AlexisBlanchard <alexis.blanchard@lacombedulionvert.fr>
2022-10-10 17:21:29 +0200

    Use Drinks over String as beverageSales key

--- src/main/java/SalesRepository.java
+++ src/main/java/SalesRepository.java
@@ -3,20 +3,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SalesRepository {
-    private Map<String, Integer> beverageSales = new HashMap<>();
+    private final Map<Drink, Integer> beverageSales = new HashMap<>();
     private float earnedMoney = 0f;
 
     public SalesRepository() {
-        EnumSet.allOf(Drink.class).forEach((drink) -> beverageSales.put(drink.toString(), 0));
+        EnumSet.allOf(Drink.class).forEach((drink) -> beverageSales.put(drink, 0));
     }
 
     public void save(Order order) {
-        beverageSales.computeIfPresent(order.getDrink().toString(), (drink, count) -> count+1);
+        beverageSales.computeIfPresent(order.getDrink(), (drink, count) -> count+1);
         earnedMoney += order.getDrink().getPrice();
     }
 
     public Integer getDrinkCount(Drink drink) {
-        return beverageSales.get(drink.toString());
+        return beverageSales.get(drink);
     }
 
     public float getEarnedMoney() {
